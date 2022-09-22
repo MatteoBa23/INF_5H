957d5806d553bca7e25d3276f6d209ef7f8e604a
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2022-09-22 15:27:30 +0200

    Add files via upload

---
+++ Ballabio_HelloWorld.c
@@ -0,0 +1,12 @@
+/****************************
+*		BALLABIO MATTEO		*
+*			   5H			*
+*		  Hello Word		*
+****************************/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("\nHello Word");
+}
