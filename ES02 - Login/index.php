8c16f7a19e59920720968d9b72bd4efb2bf89b9a
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2022-10-14 16:40:01 +0200

    Versione 1.0

---
+++ ES02 - Login/index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>ITCS Erasmo da Rotterdam</title>
+    </head>
+    <body>
+        <h2>Richiesta credenziali per l'accesso all'area riservata</h2>
+
+        <form name="frmLogin" action="login.php" method="POST">
+        Username: <input type="text" name="username"><br>
+        Password: <input type="password" name="password"><br>
+        <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
