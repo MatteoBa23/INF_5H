f3a3db63f4ffa7bd98238ee226fd54137d49042f
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2022-10-18 12:59:36 +0200

    Versione 1.0

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Esempio</title>
+</head>
+<body>
+<h1>Home Page - Matteo Ballabio</h1>
+<a href="Informatica/ES01 - TavolaPitagorica/tavola.php" target="_blank">ES01 - TavolaPitagorica</a><br>
+<!--- <a href="Informatica/ES02 - LoginBello/autenticazione.php" target="_blank">ES02 - Pagina Riservata Bella</a><br> --->
+<a href="Informatica/ES02 - Login/PaginaRiser.php" target="_blank">ES02 - Login</a><br>
+<a href="Informatica/ES03 - LoginIntero/index.php" target="_blank">ES03 - LoginIntero</a><br>
+<a href="Informatica/ES04 - Cookie/index.php" target="_blank">ES04 - Cookie</a><br>
+</body>
+</html>
