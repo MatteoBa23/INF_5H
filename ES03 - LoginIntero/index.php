8a9432d6ff77acab67cbd26c3132bbb6db38122c
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2022-10-18 13:02:01 +0200

    Versione 1.0

--- ES03 - LoginIntero/index.php
+++ ES03 - LoginIntero/index.php
@@ -1,3 +1,4 @@
+<!-- BALLABIO MATTEO -->
 <html>
 <head><title>ITCS Erasmo da Rotterdam</title>
 </head>
@@ -8,8 +9,9 @@
 	
 	if(isset($_POST["username"])){
 		$username = $_POST["username"];
+		$cognome  = $_POST["cognome"];
 		$password = $_POST["password"];
-	if($username!="admin" || $password!="password") {
+	if($username!="admin" || $password!="password" || $cognome!="rossi") {
 ?>
 	<h2>Attenzione! Username o password errate</h2>
 <?php
@@ -20,9 +22,10 @@
 }else {
 ?>
 	<form name="frmLogin" action="index.php" method="POST">
-	Username: <input type="text" name="username"><br>
-	Password: <input type="password" name="password"><br>
-	<input type="submit" value="Submit">
+	Username: <input type="text" name="username" placeholder="username"><br><br>
+	Cognome: <input type="text" name="cognome" placeholder="rossi"><br><br>
+	Password: <input type="password" name="password" placeholder="password"><br><br>
+	<input type="submit" value="Invio">
 	</form>
 <?php
 }
