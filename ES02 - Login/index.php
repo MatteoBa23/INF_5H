dafb2c3fd19207ece73e5abfc461a8fa49e5ece6
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2022-10-18 13:00:40 +0200

    Versione 1.0

--- ES02 - Login/index.php
+++ ES02 - Login/index.php
@@ -1,14 +1,15 @@
 <html>
-    <head>
-        <title>ITCS Erasmo da Rotterdam</title>
-    </head>
-    <body>
-        <h2>Richiesta credenziali per l'accesso all'area riservata</h2>
+<head>
+<title>ITCS Erasmo da Rotterdam</title>
+</head>
+<body>
+<h2>Inserisci le credenziali</h2>
 
-        <form name="frmLogin" action="login.php" method="POST">
-        Username: <input type="text" name="username"><br>
-        Password: <input type="password" name="password"><br>
-        <input type="submit" value="Submit">
-        </form>
-    </body>
+<form name="frmLogin" action="login.php" method="POST">
+Username: <input type="text" name="username" placeholder="username"><br><br>
+Cognome: <input type="text" name="cognome" placeholder="rossi"><br><br>
+Password: <input type="password" name="password" placeholder="password"><br><br>
+<input type="submit" value="Invio">
+</form>
+</body>
 </html>
