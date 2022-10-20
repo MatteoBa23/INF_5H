8fb3ebf4d438e8b4c231af09ed9c87e34e3b83a3
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2022-10-20 11:14:44 +0200

    Versione 1.0

--- ES02 - Login/login.php
+++ ES02 - Login/login.php
@@ -4,18 +4,15 @@
 </head>
 <body>
 <h2>Controllo Credenziali</h2>
-
 <?php
 $username = $_POST["username"];
-$cognome = $_POST["cognome"];
 $password = $_POST["password"];
-if($username!="admin" || $password!="password" || $cognome!="rossi") {
+if($username!="admin" || $password!="password") {
 	?>
 	<h2>Attenzione! Username o password errate</h2><br>
 	<?php
 } else {
 	echo "<h2>Benvenuto " . $username . " nell'area riservata!</h2>";
-	
 }
 ?>
 </body>
