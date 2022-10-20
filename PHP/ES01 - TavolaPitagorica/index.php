edfa3e1a5ab2f64896b42aa3a0946b65abf20015
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2022-10-20 11:18:34 +0200

    Versione 1.0

---
+++ PHP/ES01 - TavolaPitagorica/index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Tavola Pitagorica</title>
+</head>
+<body>
+<h1>Tavola Pitagorica - Ballabio Matteo</h1>
+<table border="1" cellspacing="3" cellpadding="2">
+<?php
+$limite = 10;
+$limite2 = 10;
+for($riga=1;$riga<=$limite;$riga++)
+{
+    echo "<tr>";
+    for($colonna=1;$colonna<=$limite2;$colonna++)
+    {
+        $valore = $riga * $colonna;
+        echo "<td align=\"center\">"; echo $valore; echo "</td>";
+    }
+    echo "</tr>";
+}
+?>
+</body>
+</html>
