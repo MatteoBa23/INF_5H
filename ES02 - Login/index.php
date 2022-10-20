216ca3e6e265bd5debebe13ffef68465b7951ed0
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2022-10-20 11:15:59 +0200

    Versione 1.0

--- ES02 - Login/index.php
+++ ES02 - Login/index.php
@@ -4,10 +4,8 @@
 </head>
 <body>
 <h2>Inserisci le credenziali</h2>
-
 <form name="frmLogin" action="login.php" method="POST">
 Username: <input type="text" name="username" placeholder="username"><br><br>
-Cognome: <input type="text" name="cognome" placeholder="rossi"><br><br>
 Password: <input type="password" name="password" placeholder="password"><br><br>
 <input type="submit" value="Invio">
 </form>
