8c21aebea4bc6a60e9d7593f7fba8f9873ad2e97
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2022-11-05 16:13:04 +0100

    Versione 1.0

---
+++ PHP/ES03 - Validazione Form/ES_A.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>ITCS Erasmo da Rotterdam</title>
+</head>
+<body>
+<h2>Esempio validazione dei campi di un form in PHP</h2>
+
+<form name="frmLogin" action="login.php" method="POST">
+Username: <input type="text" name="username" placeholder="username"><br><br>
+Cognome: <input type="text" name="cognome" placeholder="cognome"><br><br>
+Data di nascita: <input type="" placeholder="data di nascita"><br><br> 
+Email: <input type="text" placeholder="email"><br><br>
+Cellulare: <input type="number" placeholder="numero di telefono"><br><br>
+Nickname: <input type="text" placeholder="nickname"><br><br>
+Password: <input type="password" name="password" placeholder="password"><br><br> 
+<input type="submit" value="Invio">
+</form>
+<br><h2>Ecco i tuoi dati inseriti in precedenza</h2>
+</body>
+</html>
