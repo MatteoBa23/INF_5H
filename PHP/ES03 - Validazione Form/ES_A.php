c6448e2cdcd1259c6ce8f89190264c84b7c63f5f
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2022-11-06 15:13:14 +0100

    Versione 1.0

--- PHP/ES03 - Validazione Form/ES_A.php
+++ PHP/ES03 - Validazione Form/ES_A.php
@@ -4,17 +4,60 @@
 </head>
 <body>
 <h2>Esempio validazione dei campi di un form in PHP</h2>
-
-<form name="frmLogin" action="login.php" method="POST">
-Username: <input type="text" name="username" placeholder="username"><br><br>
-Cognome: <input type="text" name="cognome" placeholder="cognome"><br><br>
-Data di nascita: <input type="" placeholder="data di nascita"><br><br> 
-Email: <input type="text" placeholder="email"><br><br>
-Cellulare: <input type="number" placeholder="numero di telefono"><br><br>
-Nickname: <input type="text" placeholder="nickname"><br><br>
-Password: <input type="password" name="password" placeholder="password"><br><br> 
+<p>* = campi obbligatori</p>
+<?php
+/*$_SERVER["PHP_SELF"] --> invia i dati del modulo alla pagina stessa, invece di passare a una pagina diversa. 
+i messaggi di errore verranno mostrati sulla stessa pagina del modulo*/
+//definisco le variabili
+$nome = $cognome = $email = $nickname = $cellulare = $datanascita = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $nome = test_input($_POST["nome"]);
+  $cognome = test_input($_POST["cognome"]);
+  $datanascita = test_input($_POST["datanascita"]);
+  $email = test_input($_POST["email"]);
+  $cellulare = test_input($_POST["cellulare"]);
+  $nickname = test_input($_POST["nickname"]);
+  $password = test_input($_POST["password"]);
+}
+function test_input($data) {
+  $data = trim($data);
+  return $data;
+}
+?>
+<form name="frmLogin" method="POST" action="<?php $_SERVER["PHP_SELF"];?>">
+Username*: <input type="text" name="nome"><br><br>
+Cognome*: <input type="text" name="cognome"><br><br>
+Data di nascita*: <input type="text" name="datanascita"><br><br> 
+Email*: <input type="text" name="email"><br><br>
+Cellulare: <input type="text" name="cellulare"><br><br>
+Nickname*: <input type="text" name="nickname"><br><br>
+Password*: <input type="password" name="password"><br><br> 
 <input type="submit" value="Invio">
 </form>
-<br><h2>Ecco i tuoi dati inseriti in precedenza</h2>
+
+<?php
+echo "<h2>Dati inseriti:</h2>";
+echo "Nome inserito: ";
+echo $nome;
+echo "<br>";
+echo "Cognome inserito: ";
+echo $cognome;
+echo "<br>";
+echo "Data di nascita: ";
+echo $datanascita;
+echo "<br>";
+echo "Email inserita: ";
+echo $email;
+echo "<br>";
+echo "Cellulare: ";
+echo $cellulare;
+echo "<br>";
+echo "Nickname: ";
+echo $nickname;
+echo "<br>";
+echo "Password: ";
+echo $password;
+echo "<br>";
+?>
 </body>
 </html>
