49c1b67523dd376eae84dfe50f758f7f53831e66
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2022-12-12 16:06:55 +0100

    Versione 1.0

---
+++ PHP/ES04 - WebApp/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Benvenuto</title>
+    </head>
+    <body>
+        <h1>WEBAPP1</h1>
+        <h2>Gestione delle sessioni nelle applicazioni web</h2>
+        <a href="riser.php">Sezione riservata del sito</a><br>
+        <a href="login.php">Login</a><br>
+    </body>
+</html>
