c57a3e3200183f34209e6e65ff09b9d38ae0937a
MatteoBallabio23 <m1n3w4r5@gmail.com>
2022-12-13 15:02:30 +0000

    Update

---
+++ SQL/prova/prova.sql
@@ -0,0 +1,8 @@
+/*Creo il database con questa riga*/
+CREATE DATABASE infrazione;
+
+/*Creo una tabella*/
+CREATE TABLE proprietario (
+ 	CodFiscale VARCHAR(20) PRIMARY KEY,
+    Cognome VARCHAR(20)
+);
