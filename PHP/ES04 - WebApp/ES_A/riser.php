528c5cf39ba797ecd5e73caade83b41021804862
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2022-12-15 11:57:20 +0100

    Versione 1.0

--- PHP/ES04 - WebApp/ES_A/riser.php
+++ PHP/ES04 - WebApp/ES_A/riser.php
@@ -1,1 +1,23 @@
+<html>
+<head>
+<title>ITCS Erasmo da Rotterdam</title>
+</head>
 
+<body>
+
+<?php
+include "functions.php";
+
+session_start();	//inizio sessione
+
+if(isset($_SESSION["username"] && empty($_SESSION["username"])))
+	echo "Benvenuto!";
+else 
+{
+	header("location: login.php");	//funzione va direttamente nella pagina indicata 
+	die();							//distrugge la sessione
+}
+?>
+
+</body>
+</html>
