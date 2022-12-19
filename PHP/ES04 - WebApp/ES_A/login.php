8e06d43bfec7381641741be230da619bd16c9077
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2022-12-19 15:42:43 +0100

    Versione 1.0

--- PHP/ES04 - WebApp/ES_A/login.php
+++ PHP/ES04 - WebApp/ES_A/login.php
@@ -1,16 +1,12 @@
-
+<?php
+include("functions.php");
+?>
 <html>
 <head>
 <title>Validazione Form</title>
 <link rel="stylesheet" href="style.css" />
 </head>
 <body>  
-
-
-<?php
-include "functions.php";
-?>
-
     <div class="tab-form">
       <div class="title-text">
         <div class="title">Login</div>
@@ -18,19 +14,16 @@ include "functions.php";
       </div>
       <div class="form-container">
         <div class="form-campi">
-          <form action="<?php ($_SERVER["PHP_SELF"]);?>" class="login" method="POST" name="frmLogin">
+          <form action="riser.php" class="login" method="POST" name="frmLogin">
             <div class="campo">
               <input type="text" placeholder="Nome*" name="nome">
-            </div>
-            <span style="color:ff1d0b;"><?php echo $errNome;?></span>
+            </div>  
             <div class="campo">
               <input type="text" placeholder="Cognome*" name="cognome">
-            </div>
-            <span style="color:ff1d0b;"><?php echo $errCognome;?></span>
+            </div>  
             <div class="campo">
               <input type="password" placeholder="Password*" name="password">
             </div>
-            <span style="color:ff1d0b;"><?php echo $errPasw;?></span>
             <div class="campo tasto">
               <div class="tasto-layer"></div>
               <input type="submit" value="Login">
@@ -38,6 +31,7 @@ include "functions.php";
           </form> 
         </div>
       </div>
+      <a href="index.php">HomePage</a>
     </div>
 </body>
 </html>
