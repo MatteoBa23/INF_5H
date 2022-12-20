c6f899acba3a72e8d9f25e630bb755979e45a695
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2022-12-20 20:32:38 +0100

    Versione 1.0

--- PHP/ES04 - WebApp/ES_A/login.php
+++ PHP/ES04 - WebApp/ES_A/login.php
@@ -1,37 +1,33 @@
-<?php
-include("functions.php");
-?>
+
 <html>
 <head>
 <title>Validazione Form</title>
 <link rel="stylesheet" href="style.css" />
 </head>
 <body>  
-    <div class="tab-form">
-      <div class="title-text">
-        <div class="title">Login</div>
-        <div class="title-login">* = campi obbligatori</div>
-      </div>
-      <div class="form-container">
-        <div class="form-campi">
-          <form action="riser.php" class="login" method="POST" name="frmLogin">
-            <div class="campo">
-              <input type="text" placeholder="Nome*" name="nome">
-            </div>  
-            <div class="campo">
-              <input type="text" placeholder="Cognome*" name="cognome">
-            </div>  
-            <div class="campo">
-              <input type="password" placeholder="Password*" name="password">
-            </div>
-            <div class="campo tasto">
-              <div class="tasto-layer"></div>
-              <input type="submit" value="Login">
-            </div>     
-          </form> 
-        </div>
-      </div>
-      <a href="index.php">HomePage</a>
-    </div>
+
+<?php
+session_start();
+if(isset($_SESSION["username"])){
+    $username = $_SESSION["username"];
+    $psw = $_SESSION["password"];
+  if ($username != "admin" || $password != "psw")
+    echo "Username o password errate";
+  else
+    header("location: riser.php");
+}
+
+
+if(!isset($_SESSION["username"]))
+{?>
+  <form name="frmLogin" action="login.php" method="POST">
+    Username: <input type="text" name="username" placeholder="username"><br><br>
+    Password: <input type="password" name="password" placeholder="password"><br><br>
+    <input type="submit" value="Invio">
+  </form><?php
+}
+
+?>
+    
 </body>
 </html>
