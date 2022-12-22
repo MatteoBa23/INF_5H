bb222fd8c7919c9bb607ac1ec7b85c6e4e216102
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2022-12-22 11:56:58 +0100

    Versione 1.0

--- PHP/ES04 - WebApp/ES_A/riser.php
+++ PHP/ES04 - WebApp/ES_A/riser.php
@@ -1,23 +1,23 @@
+<?php
+session_start();	//inzio sessione o prendo quella esistente
+?>
 <html>
 <head>
 <title>ITCS Erasmo da Rotterdam</title>
 </head>
-
 <body>
-
 <?php
-session_start();	//inizio sessione
-
-
 if(!isset($_SESSION["username"]) && empty($_SESSION["username"]))
 {
-    echo "Non hai effettuato il "; ?>
-    <a href="login.php">login</a><?php
+	echo "Non hai effettuato il "; ?>
+	<a href="login.php">login</a><?php
 }else
 {
-    echo "Benvenuto ".$_SESSION["username"];?>
-    <a href="logout.php">Logout</a><?php
-}?>
-    
+	echo "Benvenuto "?><br>
+	<a href="logout.php">Logout</a><?php
+}
+?>
+<br>
+<a href="index.php">Home page</a><br>
 </body>
 </html>
