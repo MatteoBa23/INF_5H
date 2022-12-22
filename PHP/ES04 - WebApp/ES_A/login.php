d28843a0d5918ff11f26574d5b8fba55bde0e8a0
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2022-12-22 11:47:45 +0100

    Versione 1.0

--- PHP/ES04 - WebApp/ES_A/login.php
+++ PHP/ES04 - WebApp/ES_A/login.php
@@ -1,33 +1,27 @@
-
 <html>
 <head>
 <title>Validazione Form</title>
 <link rel="stylesheet" href="style.css" />
 </head>
 <body>  
-
 <?php
-session_start();
-if(isset($_SESSION["username"])){
-    $username = $_SESSION["username"];
-    $psw = $_SESSION["password"];
-  if ($username != "admin" || $password != "psw")
-    echo "Username o password errate";
-  else
-    header("location: riser.php");
+session_start();	//avvio sessione
+if(isset($_POST["username"])){
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+if($username!="admin" || $password!="psw")
+	echo "Nome utente o password errate";
+else
+	header("location: riser.php");
 }
-
-
 if(!isset($_SESSION["username"]))
 {?>
-  <form name="frmLogin" action="login.php" method="POST">
+  <form name="frmLogin" action="login.php" method="post">
     Username: <input type="text" name="username" placeholder="username"><br><br>
     Password: <input type="password" name="password" placeholder="password"><br><br>
     <input type="submit" value="Invio">
   </form><?php
 }
-
 ?>
-    
 </body>
 </html>
