d77d65a79e46ae92976a420bc0a30decd3858716
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2023-01-23 13:03:37 +0100

    Versione 1.

---
+++ PHP/ES05 - ConnessioneDatabase/riser.php
@@ -0,0 +1,23 @@
+<?php
+session_start();	//inzio sessione o prendo quella esistente
+?>
+<html>
+<head>
+<title>ITCS Erasmo da Rotterdam</title>
+</head>
+<body>
+<?php
+if(!isset($_SESSION["username"]) && empty($_SESSION["username"]))
+{
+	echo "Non hai effettuato il "; ?>
+	<a href="login.php">login</a><?php
+}else
+{
+	echo "Benvenuto "?><br>
+	<a href="logout.php">Logout</a><?php
+}
+?>
+<br>
+<a href="index.php">Home page</a><br>
+</body>
+</html>
