cc99530c48f19fadb6b1c9817ea545c696f32944
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2023-01-24 13:06:58 +0100

    Versione 1.0

---
+++ PHP/ES05 - ConnessioneDatabase/signup.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+<title>ITCS Erasmo da Rotterdam</title>
+</head>
+
+<body>
+<h2>Registrazione utente</h2>
+
+<form name="frmLogin" action="login.php" method="post">
+    Email: <input type="text" name="email" placeholder="email"><br><br>
+    Password: <input type="password" name="password" placeholder="password"><br><br>
+	Conferma password: <input type="password" name="confpassword" placeholder="conferma password"><br><br>
+    <input type="submit" value="Invio">
+</form>
+
+
+</body>
+</html>
