60304c00a079ae5ddefb99042f3526e38cbff0bc
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2023-01-26 17:14:35 +0100

    Versione 1.0

--- PHP/ES05 - ConnessioneDatabase/index.php
+++ PHP/ES05 - ConnessioneDatabase/index.php
@@ -2,13 +2,9 @@
 <head>
 <title>ITCS Erasmo da Rotterdam</title>
 </head>
-
 <body>
-
 <h2>Benvenuto nel nostro sito</h2>
 <a href="riser.php">Sezione riservata del sito</a><br>
 <a href="login.php">Login</a><br>
-
-
 </body>
 </html>
