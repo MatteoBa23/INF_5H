1fb73f68533568e8188f18d6a7e92c4403754403
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2023-01-30 15:30:52 +0100

    Versione 1.0

---
+++ PHP/ES05 - ConnessioneDatabase/function.php
@@ -0,0 +1,30 @@
+<?php
+
+    function login(){
+
+    //variabili passate dal metodo POST del login
+    $username = $_POST['username'];
+    $psw = $_POST['password'];
+
+    //preparo la query al database
+    $pdo = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
+
+    //controlla se nel database esistono sia l'username che la password inserite precedentemente
+    $query = "SELECT * FROM utente WHERE username='$username' AND psw='$psw'";
+    $result = $pdo->query($query);
+
+    //controllo se la variabile ha acquisito le informazioni dal database
+    if ($result->rowCount() > 0){
+        //esito positivo
+        $_SESSION['login'] = true;
+        echo "<h3>Benvenuto utente </h3><br>";
+        echo "<a href='riser.php'>Pagina riservata</a>";
+    }
+    else{
+        //esito negativo
+        $_SESSION['login'] = false;
+        echo "Nome utente o password errate";
+    }
+    }
+    
+?>
