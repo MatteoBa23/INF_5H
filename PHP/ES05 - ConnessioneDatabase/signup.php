3597815c2669a70b312608238222674b5333476d
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2023-01-31 13:05:40 +0100

    Versione 1.0

--- PHP/ES05 - ConnessioneDatabase/signup.php
+++ PHP/ES05 - ConnessioneDatabase/signup.php
@@ -1,14 +1,32 @@
+<?php
+session_start();
+require 'function.php';
+?>
 <html>
 <head>
 <title>ITCS Erasmo da Rotterdam</title>
 </head>
 <body>
+<?php
+/*
+//definizioni info per il database
+define('DB_SERVER', 'localhost');
+define('DB_NAME', 'gestione_utente');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+*/
+?>
 <h2>Registrazione utente</h2>
 <form name="frmLogin" action="login.php" method="post">
-    Username: <input type="text" name="Username" placeholder="Username"><br><br>
+    Username: <input type="text" name="username" required pattern="/^[a-zA-Z0-9_@#]*$/"><br><br>
     Password: <input type="password" name="password" placeholder="password"><br><br>
 	Conferma password: <input type="password" name="confpassword" placeholder="conferma password"><br><br>
     <input type="submit" value="Invio">
 </form>
+<?php
+/*
+registra();
+*/
+?>
 </body>
 </html>
