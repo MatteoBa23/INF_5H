9ea8486c93f115e113947b18584da76e3f576a5d
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2023-02-02 12:00:54 +0100

    Update istituzione.sql

--- SQL/istituzione.sql
+++ SQL/istituzione.sql
@@ -6,18 +6,18 @@ CREATE DATABASE istituzione;
 
 //creazione tabella studenti
 CREATE TABLE studenti(
-	matricola CHAR(10) PRIMARY KEY,
+    matricola CHAR(10) PRIMARY KEY,
     nome CHAR(15) NOT NULL,
     cognome CHAR(15) NOT NULL,
-    data_nascita DATE NOT NULL,
     codice_scuola INT NOT NULL,
     FOREIGN KEY(codice_scuola) REFERENCES scuola(codice)
-        ON UPDATE CASCADE
+        ON DELETE SET DEFAULT ON UPDATE CASCADE
 );
 
 //creazione tabella scuola
 CREATE TABLE scuola (
-    codice INT NOT NULL AUTO_INCREMENT PRIMARY KEY 
+    codice INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    codice_scuola VARCHAR(20)
 );
 
 
@@ -35,14 +35,20 @@ ALTER TABLE studenti DROP COLUMN nome
 
 //inserisco i valori nei campi 
 INSERT
-INTO studenti (matricola, nome, cognome, data_nascita, codice_scuola)
-VALUES ('D5RF8','Gianno','Giannotti','1998/06/12','2')
+INTO studenti (matricola, nome, cognome, codice_scuola)
+VALUES ('D5RF8','Gianno','Giannotti','01');
 
 //elimino istanze
 DELETE FROM studenti        //elimino la riga con il nome = 'Antonio'
 WHERE nome='Antonio';
 
 //aggiorno informazioni
-UPDATE studenti                //aggiorno il cognome solo nella riga con il cognome = 'Giannotti'
+UPDATE studenti                
 SET voto = '3'
 WHERE cognome='Giannotti';
+
+
+//istruzione congiunzione tabelle
+SELECT st.cognome, sc.nome_scuola
+FROM studenti as st, scuola as sc
+WHERE st.codice_scuola=sc.codice
