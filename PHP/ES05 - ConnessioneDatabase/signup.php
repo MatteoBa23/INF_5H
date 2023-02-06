5cdb49e058bad9427709fb88057e1427c63eb5bc
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2023-02-06 13:06:16 +0100

    Update signup.php

--- PHP/ES05 - ConnessioneDatabase/signup.php
+++ PHP/ES05 - ConnessioneDatabase/signup.php
@@ -8,25 +8,24 @@ require 'function.php';
 </head>
 <body>
 <?php
-/*
 //definizioni info per il database
 define('DB_SERVER', 'localhost');
 define('DB_NAME', 'gestione_utente');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
-*/
 ?>
 <h2>Registrazione utente</h2>
 <form name="frmLogin" action="login.php" method="post">
     Username: <input type="text" name="username" required pattern="/^[a-zA-Z0-9_@#]*$/"><br><br>
-    Password: <input type="password" name="password" placeholder="password"><br><br>
-	Conferma password: <input type="password" name="confpassword" placeholder="conferma password"><br><br>
+    Password: <input type="password" name="password"><br><br>
+	Email: <input type="text" name="email"><br><br>
+	Data di nascita: <input type="text" name="data_nascita"><br><br>
     <input type="submit" value="Invio">
 </form>
 <?php
-/*
-registra();
-*/
+
+signup();
+
 ?>
 </body>
 </html>
