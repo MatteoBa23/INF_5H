62b00e446e47c4d0212da2947f572e9cb18a433d
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2023-02-07 13:07:24 +0100

    Update signup.php

--- PHP/ES05 - ConnessioneDatabase/signup.php
+++ PHP/ES05 - ConnessioneDatabase/signup.php
@@ -13,19 +13,31 @@ define('DB_SERVER', 'localhost');
 define('DB_NAME', 'gestione_utente');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
-?>
-<h2>Registrazione utente</h2>
-<form name="frmLogin" action="login.php" method="post">
-    Username: <input type="text" name="username" required pattern="/^[a-zA-Z0-9_@#]*$/"><br><br>
-    Password: <input type="password" name="password"><br><br>
-	Email: <input type="text" name="email"><br><br>
-	Data di nascita: <input type="text" name="data_nascita"><br><br>
-    <input type="submit" value="Invio">
-</form>
-<?php
 
-signup();
 
+if(!isset($_SESSION['login']))
+{
+	if(!isset($_POST['Submit'])){?>
+  		<h2>Registrazione utente</h2>
+		<form name="frmLogin" action="<?=$_SERVER['PHP_SELF']?>" method="post">
+			Nome: <input type="text" name="nome"><br><br>
+			Cognome: <input type="text" name="cog"><br><br>
+			Username: <input type="text" name="username"><br><br>
+			Password: <input type="password" name="password"><br><br>
+			Email: <input type="text" name="email"><br><br>
+			Data di nascita: <input type="date" name="data_nascita"><br><br>
+		<input type="submit" name="Submit" value="Invio">
+		</form><?php
+	}else{
+		signup();
+		echo "Vai alla pagina riservata <br>";
+		echo "<a href='riser.php'>Pagina Riservata</a><br>";
+	}
+			
+}
 ?>
+
+
+
 </body>
 </html>
