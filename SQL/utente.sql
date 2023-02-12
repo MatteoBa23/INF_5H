83c5223d4c04e979aa9f5d4240063ab07dea1989
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2023-02-12 15:11:33 +0100

    Update utente.sql

--- SQL/utente.sql
+++ SQL/utente.sql
@@ -3,8 +3,12 @@ CREATE DATABASE Gestione_Utente;
 
 CREATE TABLE utente (
   id INT NOT NULL AUTO_INCREMENT,
-  username VARCHAR(65) NOT NULL,
+  nome VARCHAR(65) NOT NULL,
+  cognome VARCHAR(65) NOT NULL,
+  username VARCHAR(65) NOT NULL UNIQUE,
   psw VARCHAR(65) NOT NULL,
+  email VARCHAR(65) NOT NULL UNIQUE,
+  data_nascita DATE NOT NULL,  
   PRIMARY KEY(id)
 ); 
 
@@ -14,8 +18,16 @@ CREATE TABLE utente (
 
 //INSERIMENTO DATI
 INSERT
-INTO utente (id,username,psw)
-VALUES (NULL,'Giannotto','provozza');
+INTO utente (id,nome,cognome,username,psw,email,data_nascita)
+VALUES (NULL,'Giacomo','Provozza','GiacoProvo','giacomino','giacomo.provozza@gmail.com','2006/02/25');
+
+INSERT
+INTO utente (id,nome,cognome,username,psw,email,data_nascita)
+VALUES (NULL,'Sofia','Esplosa','SofiEsplo','sofia','sofia.esplosa@gmail.com','2007/08/12');
+
+INSERT
+INTO utente (id,nome,cognome,username,psw,email,data_nascita)
+VALUES (NULL,'Marco','Vino','MarcoVino','marcolino','marco.vino@gmail.com','2002/04/02');
 
 //AGIORNO TABELLE
 UPDATE utente
