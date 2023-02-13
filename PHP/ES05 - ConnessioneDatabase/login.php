55e9be2c4862cfd863f9db9914059f315315dbfa
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2023-02-13 12:58:39 +0100

    Update login.php

--- PHP/ES05 - ConnessioneDatabase/login.php
+++ PHP/ES05 - ConnessioneDatabase/login.php
@@ -8,35 +8,34 @@ require 'function.php';
 </head>
 <body>  
 <?php
-//definizioni info del database
+//definizioni info per il database
 define('DB_SERVER', 'localhost');
 define('DB_NAME', 'gestione_utente');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 
-//controllo se esiste la variabile di sessione login
+//controllo login è già statto controllato
 if(!isset($_SESSION['login']))
 {
-	//controllo se l'utente ha già effettuato il login
-	if(!isset($_POST['Login']))
-	{?>
+	//se non è già stato effettuato il login compare il form
+	if(!isset($_POST['Login'])){?>
   		<form name="frmLogin" action="login.php" method="POST">
 		Username: <input type="text" name="username" placeholder="username"><br><br>
    		Password: <input type="password" name="password" placeholder="password"><br><br>
+		<p>Nont ti ricordi la password? Cambiala<p><a href="reset.php">qui</a><br>
 		<p>Non hai effettuato la registrazione? Registrati<p><a href="signup.php">qui</a><br><br>
    		<input type="submit" name="Login">
  		</form><?php
 	}else
+		//verifico le credenziali inserite, utente già fatto il login
 		login();	
-}else
-{
+}else {
+	//se utente ha già fatto il login, compare link per alcune pagine
 	echo "Hai già effettuato il login <br>";
 	echo "<a href='index.php'>Home page</a><br>";
 	echo "<a href='logout.php'>Logout</a><br>";
 	echo "<a href='riser.php'>Pagina riservata</a><br>";
 }
-	
-
 ?>
 </body>
 </html>
