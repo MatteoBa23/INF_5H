655f3ec7123709714ae9fe7fd789cf9896b0579b
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2023-03-02 11:36:13 +0100

    Update istituzione.sql

--- SQL/istituzione.sql
+++ SQL/istituzione.sql
@@ -38,8 +38,13 @@ INSERT
 INTO studenti (matricola, nome, cognome, codice_scuola)
 VALUES ('D5RF8','Gianno','Giannotti','01');
 
+INSERT
+INTO studenti (matricola, nome, cognome, codice_scuola, data_nascita, email, codice_rapp)
+VALUES ('','','','','','','');
+
+
 //elimino istanze
-DELETE FROM studenti        //elimino la riga con il nome = 'Antonio'
+DELETE FROM studenti        
 WHERE nome='Antonio';
 
 //aggiorno informazioni
@@ -54,6 +59,11 @@ SELECT st.cognome, sc.nome_scuola
 FROM studenti as st, scuola as sc
 WHERE st.codice_scuola=sc.codice
 
+//SELF JOIN
+SELECT s1.nome, s1.cognome, s2.cognome AS rappresentante
+FROM studenti AS s1, studenti AS s2
+WHERE s1.codice_rapp = s2.matricola
+
 //join a sinistra
 SELECT st.matricola, st.cognome, sc.nome_scuola
 FROM studenti AS st LEFT JOIN scuola AS sc 
@@ -67,3 +77,131 @@ ON st.codice_scuola = sc.codice
 SELECT st.matricola, st.cognome, sc.nome_scuola
 FROM studenti AS st INNER JOIN scuola AS sc 
 ON st.codice_scuola = sc.codice
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CIAO
