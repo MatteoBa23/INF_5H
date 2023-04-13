20bb3255654cbe0d2aad90dfa0d9c6d807b55bae
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2023-04-13 11:49:21 +0200

    Update index.php

--- PHP/ES08 - ShowDatabase/ES_A/index.php
+++ PHP/ES08 - ShowDatabase/ES_A/index.php
@@ -18,23 +18,15 @@ $pdo = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASSWORD)
 	
 //controlla se nel database esistono sia l'username che la password inserite precedentemente
 $query = "SELECT * FROM utente";
-//$result = $pdo->query($query);
+$result = $pdo->query($query);
 	
 //visualizzo il database
-
+foreach($result as $riga){
+	print $riga["id"] ."". $riga["nome"] ."". $riga["cognome"];
+}
     
 	
-$limite = 3;
-$limite2 = 3;
-for($riga=1;$riga<=$limite;$riga++)
-{
-    echo "<tr>";
-    for($colonna=1;$colonna<=$limite2;$colonna++)
-    {
-        echo "<td align=\"center\">"; echo $query; echo "</td>";
-    }
-    echo "</tr>";
-}
+
 
 ?>
 </body>
