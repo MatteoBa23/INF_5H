d2b17868dc43debf6c84ff98fd508b2430639954
MatteoBallabio23 <113595415+MatteoBa23@users.noreply.github.com>
2023-04-14 14:26:27 +0200

    Update show_table.php

--- PHP/ES08 - ShowDatabase/show_table.php
+++ PHP/ES08 - ShowDatabase/show_table.php
@@ -7,8 +7,9 @@
 <h2>Visualizzazione del Database</h2>
 <table border="1" cellspacing="3" cellpadding="2">
 <?php
+session_start();
 define('DB_SERVER', 'localhost');
-define('DB_NAME', 'visualizzo_db');
+define('DB_NAME', 'show_db');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 
@@ -19,12 +20,18 @@ $pdo = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASSWORD)
 //controlla se nel database esistono sia l'username che la password inserite precedentemente
 $query = "SELECT * FROM utente";
 $result = $pdo->query($query);
-	
+
+echo "<h3>Se vuoi ordine in ordine alfabetico o in ordine crescente una colonna, devi cliccare sulla colonna interessata</h3>";
+echo "<tr>";
+echo "<td align=\"center\"> <a href='show_ordered_table.php'>ID</a> <td align=\"center\"> <a href='show_ordered_table.php'>Nome</a>
+<td align=\"center\"> <a href='show_ordered_table.php'>Cognome</a>";
+echo "</tr>";
+
 //visualizzo il database
 foreach($result as $riga){
 	echo "<tr>";
 	echo "<td align=\"center\">"; 
-	print $riga["id"] ."<td align=\"center\"> ". $riga["nome"] ."<td align=\"center\">". $riga["cognome"] ."</td>";
+	print $riga["id"] ."<td align=\"center\">". $riga["nome"] ."<td align=\"center\">". $riga["cognome"] ."</td>";
 	echo "</tr>";
 }
 ?>
